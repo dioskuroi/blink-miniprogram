6d3b27e5b1492de05a0d969599845f3e76c70b95
徐俊 <xujun1019@hotmail.com>
2020-02-13 21:18:17 +0800

    feat: music

---
+++ components/classic/music/index.js
@@ -0,0 +1,20 @@
+// components/classic/music/index.js
+import classicBehavior from '../classic-behavior.js'
+Component({
+  behaviors: [classicBehavior],
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    pauseSrc: 'images/player@pause.png',
+    playSrc: 'images/player@play.png'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
