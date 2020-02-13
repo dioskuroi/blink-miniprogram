bb7ca649e81bcf9c5dd9d0b660e54d988f960acf
徐俊 <xujun1019@hotmail.com>
2020-02-13 21:20:36 +0800

    feat: 缓存 classic 数据

--- models/Classic.js
+++ models/Classic.js
@@ -1,9 +1,45 @@
 import Http from '../util/Http.js'
+import { getStorage, setStorage } from '../util/util.js'
 
 export default class ClassicModel extends Http {
   getLatest() {
     return this.request({
       url: 'classic/latest',
+    })
+  }
+
+  getPrev(index) {
+    return this._getClassic(index, -1)
+  }
+
+  getNext(index) {
+    return this._getClassic(index, 1)
+  }
+
+  _getClassic(curIndex, step) {
+    return new Promise(resolve => {
+      const key = this._getKey(curIndex + step)
+      console.log(key)
+      getStorage(key).then(({ data }) => {
+        resolve(data)
+      }).catch(() => {
+        this.request({
+          url: `classic/${curIndex}/${step > 0 ? 'next' : 'previous'}`
+        }).then(data => {
+          resolve(data)
+        })
+      })
+    })
+  }
+
+  _getKey(index) {
+    return `classic-${index}`
+  }
+
+  resHandler({ data }, resolve) {
+    const key = this._getKey(data.index)
+    setStorage(key, data).then(() => {
+      resolve(data)
     })
   }
 }
