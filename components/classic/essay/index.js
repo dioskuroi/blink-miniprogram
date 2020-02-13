2d8de961dadbd518bf0440f7740f1a0dc0490483
徐俊 <xujun1019@hotmail.com>
2020-02-13 21:18:06 +0800

    feat: essay

---
+++ components/classic/essay/index.js
@@ -0,0 +1,19 @@
+// components/classic/essay/index.js
+import classicBehavior from '../classic-behavior.js'
+
+Component({
+  behaviors: [classicBehavior],
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
