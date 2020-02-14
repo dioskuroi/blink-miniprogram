71dd34c3f7d80a532e4498b49e486cebbe8a6463
徐俊 <xujun1019@hotmail.com>
2020-02-14 20:57:36 +0800

    完成music组件c

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,5 +1,8 @@
 // components/classic/music/index.js
 import classicBehavior from '../classic-behavior.js'
+
+const mMgr = wx.getBackgroundAudioManager() 
+
 Component({
   behaviors: [classicBehavior],
 
@@ -11,14 +14,62 @@ Component({
    * 组件的初始数据
    */
   data: {
+    playing: false,
     pauseSrc: 'images/player@pause.png',
     playSrc: 'images/player@play.png'
+  },
+
+  attached() {
+    this.recoverStatus()
+    this.monitorSwitch()
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    onPlay() {
+      let { playing, src } = this.data
+      playing = !playing
+      if (playing) {
+        mMgr.title = src
+        mMgr.src = src
+      } else {
+        mMgr.pause()
+      }
+      this.setData({
+        playing
+      })
+    },
+    recoverStatus() {
+      if (mMgr.paused) {
+        this.setData({
+          playing: false
+        })
+        return
+      }
+      if (mMgr.src === this.data.src) {
+        this.setData({
+          playing: true
+        })
+      }
+    },
+    monitorSwitch() {
+      mMgr.onPlay(() => {
+        this.recoverStatus()
+      })
 
+      mMgr.onPause(() => {
+        this.recoverStatus()
+      })
+
+      mMgr.onEnded(() => {
+        this.recoverStatus()
+      })
+
+      mMgr.onStop(() => {
+        this.recoverStatus()
+      })
+    }
   }
 })
