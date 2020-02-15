c15ffd9f32d146b51a4b79409b849083290bdf40
徐俊 <xujun1019@hotmail.com>
2020-02-15 20:57:57 +0800

    feat: book-detail 静态页面

--- models/Book.js
+++ models/Book.js
@@ -6,4 +6,28 @@ export default class BookModel extends Http {
       url: 'book/hot_list'
     })
   }
+
+  getMyBookCount() {
+    return this.request({
+      url: 'book/favor/count'
+    })
+  }
+
+  getDetail(bid) {
+    return this.request({
+      url: `book/${bid}/detail`
+    })
+  }
+
+  getLikeStatus(bid) {
+    return this.request({
+      url: `book/${bid}/favor`
+    })
+  }
+
+  getComments(bid) {
+    return this.request({
+      url: `book/${bid}/short_comment`
+    })
+  }
 }
